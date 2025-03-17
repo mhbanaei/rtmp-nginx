45fdbe21d39abf27c76ec4074cdd40596ea812f2
unknown <hossein711@gmail.com>
2025-03-16 21:06:07 -0700

    prototype Version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # rtmp-nginx
 Pushing RTMP data as Restreaming Service 
+
+Requirement Package:
+
+	Microsoft.Visual.C.All.Package
+	nginx 1.7.11.3 Gryphon
+	http://nginx-win.ecsds.eu/download/
+
+Requirement For auth_server.py
+	pip install flask
+
+# Stream Key Validation API
+A Flask-based APi to validate stream keys for different servers 
+
+# How It Works 
+ Endpoint: POST /auth
+ Params:
+	server : Server name (query parameter)
+	name : Stream Key (form data)
+Validates if the stream key matches the one for the specified serve.
+
+# Example 
+	Request : POST /auth?server=live1 with name=key1_live1
+	Response : 200 OK for valid Key, 403 Forbidden for invalid key.
+	
+# Running 
+
+Very Simple :
+	install the Requirement Package then : 
+		py auth_server.py 
+# note
+	Tested on For Windows Server 2022 
---
+++ auth_server.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# Tarif Data name va valid Stream Key 
+VALID_KEYS = {
+    "live1": ["key1_live1", "key2_live1"],
+    "live2": ["key1_live2", "key2_live2"],
+    "live3": ["key1_live3", "key2_live3"]
+}
+
+@app.route("/auth", methods=["POST"])
+def auth():
+    server = request.args.get("server", "")  # Server name based on Query String
+    stream_key = request.form.get("name", "")  # Streamkey Form Data
+
+    if server in VALID_KEYS and stream_key in VALID_KEYS[server]:
+        print(f"✅ Access granted to {server} with key {stream_key}")
+        return "OK", 200  # Mojaz Be Stream
+    else:
+        print(f"❌ Access denied for {server} with key {stream_key}")
+        return "Forbidden", 403  # Decline Kardan Stream 
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
